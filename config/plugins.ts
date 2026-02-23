28b52630a920851d45c74acdd4e6be72533e8a65
niharika.annepaka <niharika.annepaka@trinitiai.in>
2026-02-23 21:59:36 +0530

    Add Cloudinary storage

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,8 +1,4 @@
-// import type { Core } from '@strapi/strapi';
 
-// const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
-
-// export default config;
 
 module.exports = ({ env }) => ({
   upload: {
