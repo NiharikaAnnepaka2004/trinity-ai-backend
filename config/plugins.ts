085341c4409c45eadab9c5ba10fe4239389b5981
niharika.annepaka <niharika.annepaka@trinitiai.in>
2026-02-24 10:53:41 +0530

    Simplify Cloudinary config and remove conflicting plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,15 +1,14 @@
-module.exports = ({ env }) => {
-  return {
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-          timeout: 60000,
-        },
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+        secure: true,
+        max_file_size: 5242880,
       },
     },
-  };
-};
+  },
+});
